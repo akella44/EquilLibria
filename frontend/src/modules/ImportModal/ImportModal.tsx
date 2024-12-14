b01767ed115c5610645d6747c0774b0070b6765a
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-14 16:22:42 +0300

    keyboard fix

---
+++ frontend/src/modules/ImportModal/ImportModal.tsx
@@ -0,0 +1,10 @@
+import { Modal } from "@/shared/ui/Modal";
+import { FC } from "react";
+
+export const ImportModal: FC = () => {
+  return (
+    <Modal>
+      <div></div>
+    </Modal>
+  );
+};
